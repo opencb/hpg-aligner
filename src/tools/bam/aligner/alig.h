0208cd372c8a6598534247ba00836ec46bc4bc98
Raúl Moreno <raulmorenogaldon@gmail.com>
2014-01-23 12:01:41 +0100

    [BAM]Implement: Removed obsolete functions

--- src/tools/bam/aligner/alig.h
+++ src/tools/bam/aligner/alig.h
@@ -116,13 +116,8 @@ EXTERNC ERROR_CODE alig_bam_file2(char *bam_path, char *ref_name, char *ref_path
  * BAM REALIGN
  */
 
-EXTERNC ERROR_CODE alig_bam_file(char *bam_path, char *ref_name, char *ref_path);
-EXTERNC ERROR_CODE alig_bam_list(array_list_t *bam_list, genome_t* ref);
-EXTERNC ERROR_CODE alig_bam_list_to_disk(array_list_t *bam_list, bam_file_t *bam_f);
-
 EXTERNC ERROR_CODE alig_bam_list_realign(array_list_t *bam_list, array_list_t *haplotype_list, genome_t* ref);
 
-EXTERNC ERROR_CODE alig_bam_list_get_next(bam_file_t *bam_f, array_list_t *out_list);
 
 
 #endif /* ALIG_H_ */
