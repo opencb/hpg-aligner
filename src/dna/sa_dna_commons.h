f02f416e48be29ec7b51dbe8d4edfdaf4e6a68c2
jtarraga <jtarraga@jtarragaPC.(none)>
2014-05-08 12:16:54 +0200

    Improves the function to compute the read covering area in a given CAL

--- src/dna/sa_dna_commons.h
+++ src/dna/sa_dna_commons.h
@@ -927,8 +927,7 @@ static inline void seed_cal_update_info(seed_cal_t *cal) {
     num_mismatches += seed->num_mismatches;
     num_open_gaps += seed->num_open_gaps;
     num_extend_gaps += seed->num_extend_gaps;
-    read_area += (seed->read_end - seed->read_start - num_mismatches - num_open_gaps - num_extend_gaps);
-    
+    read_area += (seed->read_end - seed->read_start - num_mismatches - (2 * num_open_gaps) - (num_extend_gaps));
   }
   cal->num_mismatches = num_mismatches;
   cal->num_open_gaps = num_open_gaps;
