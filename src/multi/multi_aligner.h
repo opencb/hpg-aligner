5e7940f52c944b19dd1476b79e18a06ce960ede2
martineh <hekthor.martinez@gmail.com>
2015-03-26 08:55:57 +0100

    Add two new files

---
+++ src/multi/multi_aligner.h
@@ -0,0 +1,28 @@
+#ifndef MULTI_ALIGNER_H
+#define MULTI_ALIGNER_H
+
+#include <mpi.h>
+
+#include <options.h>
+
+int hpg_multialigner_main(options_t *options);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
